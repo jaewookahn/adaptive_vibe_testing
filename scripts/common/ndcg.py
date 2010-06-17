7882d1fe815e111ddba0ac46c45aff09b602b26a
jaewookahn <jaewook.ahn@gmail.com>
2010-06-17 01:36:38 +0000

    added ndcg for sys prec

--- scripts/common/ndcg.py
+++ scripts/common/ndcg.py
@@ -1,12 +1,35 @@
 import math
 
-def get_ndcg(rels):
+# def get_ndcg(rels):
+# 	
+# 	if len(rels) == 0:
+# 		return 0.0
+# 	
+# 	ndcg = rels[0]
+# 	for i, r in enumerate(rels[1:]):
+# 		temp = float(r) / math.log(i+2,2)
+# 		ndcg += temp
+# 	return ndcg
 	
+	
+def get_ndcg(rels):
 	if len(rels) == 0:
 		return 0.0
-	
-	ndcg = rels[0]
+
+	dcg = rels[0]
 	for i, r in enumerate(rels[1:]):
 		temp = float(r) / math.log(i+2,2)
-		ndcg += temp
-	return ndcg
+		dcg += temp
+
+	if dcg == 0:
+		return 0.0
+
+	irels = rels
+	irels.sort()
+	irels.reverse()
+	idcg = irels[0]
+	for i, r in enumerate(irels[1:]):
+		temp = float(r) / math.log(i+2,2)
+		idcg += temp
+
+	return dcg/idcg	
